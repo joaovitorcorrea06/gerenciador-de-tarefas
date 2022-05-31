fafbaf623f11d81a37303e17caa513e992a4ba7f
João Vitor Corrêa <70239873+joaovitorcorrea06@users.noreply.github.com>
2022-05-31 08:06:10 -0300

    .

---
+++ src/listar/itens-lista-tarefas.test.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import ItensListaTarefas from './itens-lista-tarefas';
+import Tarefa from '../models/tarefa.model';
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
--- src/listar/listar-tarefas.js
+++ src/listar/listar-tarefas.js
@@ -1,10 +1,52 @@
 import { Link } from "react-router-dom";
+import { Table } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPlus } from "@fortawesome/free-solid-svg-icons"
+import { useEffect, useState } from "react";
+import ItensListaTarefas from "./itens-lista-tarefas";
 
 function ListarTarefas () {
+
+    const [tarefas, setTarefas] = useState([]);
+    const [carregarTarefas, setCarregarTarefas] = useState(true);
+
+    useEffect(() => {
+        function obterTarefas() {
+            const tarefasDb = localStorage['tarefas'];
+            let listarTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+            setTarefas(listarTarefas);
+        }
+        if (carregarTarefas){
+            obterTarefas();
+            setCarregarTarefas(false);
+        }
+    }, [carregarTarefas]);
+
     return(
-        <Link to='/cadastro' className="btn btn-success btn-sm">
-            Nova tarefa
-        </Link>   
+        <div className="text-center">
+            <h3>Tarefas a fazer</h3>
+            <Table striped bordered hover responsive data-testid="tabela">
+                <thead>
+                    <tr>
+                        <th>Tarefa</th>
+                        <th>
+                        <Link to='/cadastro' className="btn btn-success btn-sm" data-testid="btn-nova-tarefa">
+                            <FontAwesomeIcon icon={faPlus} />
+                            &nbsp;
+                            Nova tarefa
+                        </Link>  
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <ItensListaTarefas 
+                        tarefas={tarefas}
+                        recarregarTarefas={setCarregarTarefas}
+                        />
+
+                </tbody>
+            </Table> 
+        </div>
     ); 
 }
 
